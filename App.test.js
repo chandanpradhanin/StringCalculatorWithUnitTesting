97ef86691a7efe34f904751afe8223dd5f2c8ee7
chandanpradhanin <chandanpradhan.in@gmail.com>
2025-01-07 02:11:18 +0530

    - Some more test cases tested and negative number test case failed.

--- App.test.js
+++ App.test.js
@@ -8,4 +8,17 @@ describe('CalculatorCases', () => {
   test('Returns the number itself if the input is a single number', () => {
     expect(add('1')).toBe(1);
   });
+
+  test('Should be able to handle an indefinite number of numbers.', () => {
+    expect(add('1,2,3,4,5')).toBe(15);
+  });
+
+  test('Should be able to support various separators', () => {
+    expect(add(';\n1;2')).toBe(3);
+    expect(add('#\n2#3#4')).toBe(9);
+  });
+
+  test('Should throw an exception if a negative number is encountered.', () => {
+    expect(() => add('-2')).toThrow('Invalid numbers -2');
+  });
 });
--- src/utils/CommonFunctions.ts
+++ src/utils/CommonFunctions.ts
@@ -1,6 +1,21 @@
-const add = (numbers: string) => {
-  if (numbers) {
-    return numbers;
+const add = (inputString: string) => {
+  if (inputString) {
+    let separator = /,|\n/;
+
+    const separatorMatch = inputString.match(/^(.+)\n/);
+    if (separatorMatch) {
+      separator = new RegExp(
+        separatorMatch[1].replace(/[.*+?^${}()|[\]\\]/g, '\\$&'),
+      );
+      inputString = inputString.slice(separatorMatch[0].length);
+    }
+
+    const numbersArray = inputString.split(separator).map(Number);
+
+    return numbersArray.reduce(
+      (total, value) => total + (isNaN(value) ? 0 : value),
+      0,
+    );
   } else {
     return 0;
   }
