8ff405eb5be363d5809faa5b4cfcd1c502355f21
chandanpradhanin <chandanpradhan.in@gmail.com>
2025-01-07 03:04:05 +0530

    - Implemented add function on sample design screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,77 @@
+import React, { useState } from 'react';
+import { View, TextInput, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import {add} from './src/utils/CommonFunctions';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-  View,
-} from 'react-native';
+const App = () => {
+  const [enteredValues, setEnteredValues] = useState('');
+  const [outcome, setOutcome] = useState<number | null>(null);
 
-function App(): React.JSX.Element {
+  const handleContinue = () => {
+    try {
+      const total = add(enteredValues);
+      setOutcome(total);
+    } catch (error: any) {
+      Alert.alert('Error', error.message);
+    }
+  };
+
   return (
-    <SafeAreaView>
-        <View>
-          <Text>{'Initial setup'}</Text>
-        </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.textInputContainer}>
+      <TextInput
+        style={styles.input}
+        placeholder="Input here "
+        value={enteredValues}
+        onChangeText={setEnteredValues}
+        multiline={true}
+      />
+      {outcome !== null && (
+        <Text style={styles.result}>Output: {outcome}</Text>
+      )}
+      </View>
+    <TouchableOpacity style={styles.touchable} onPress={handleContinue}>
+      <Text style={styles.text}>{'Continue'}</Text>
+      </TouchableOpacity>
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 16,
+  },
+  textInputContainer:{
+    height:50,
+    width:'100%',
+    margin:5,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+  },
+  touchable:{
+    backgroundColor:'green',
+    height:50,
+    padding: 16,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color:'white',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '70%',
+    paddingHorizontal: 8,
+  },
+  result: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
