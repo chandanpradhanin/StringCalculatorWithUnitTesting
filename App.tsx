c58400507930c305b85d15be969748a06efa16d8
chandanpradhanin <chandanpradhan.in@gmail.com>
2025-01-06 23:57:54 +0530

    - Initial commit with project creation

---
+++ App.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+        <View>
+          <Text>{'Initial setup'}</Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
